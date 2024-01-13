241fa7d378c8b7b5e67255ad8fb33f6ad7506db6
ghzaiell <148453028+ghzaiell@users.noreply.github.com>
2024-01-13 12:06:12 +0100

    Add files via upload

---
+++ car.js
@@ -0,0 +1,10 @@
+function carpublie() {
+document.write("votre publication ete envoyer a l'adminisateur");
+
+}
+
+
+function msgenvoye() {
+
+document.getElementById("msg").innerText+=(" : msg envoyee avec succès")    
+}
